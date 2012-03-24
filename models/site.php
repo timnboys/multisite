845140d44e3b7706d7a754dae35d4ce9822ac5a8
David Strack <davidstrack@gmail.com>
2012-03-24 17:07:16 -0500

    new field logic

--- models/site.php
+++ models/site.php
@@ -5,13 +5,34 @@ Loader::library('pavement/model', 'multisite');
 
 class Site extends PavementModel {
 	
+	public function getFields() {
+		return array(
+			'url' => array(
+				'label' => 'URL',
+				'type' => 'text',
+				'required' => true
+			),
+			'home_id' => array(
+				'label' => 'Home Page',
+				'type' => 'page',
+				'required' => true
+			),
+			'favicon' => array(
+				'label' => 'Favicon',
+				'type' => 'file',
+				'required' => false
+			)
+		);
+	}
+	
 	public function create($data) {
 		$url = str_replace(array('http://','https://', 'www.'), '', $data['url']);
-		$page = $this->createPage('/sites', $data['page_type'], $data['title'], $url);
+		// $page = $this->createPage('/sites', $data['page_type'], $data['title'], $url);
 		if (is_object($page)) {
 			$this->save(array(
 				'url' => $url,
-				'home_id' => $page->getCollectionID()
+				'home_id' => $data['home_id'],
+				'favicon' => $data['favicon']
 			));	
 		}
 	}
