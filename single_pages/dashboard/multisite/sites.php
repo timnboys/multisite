d40bf6e1f0c29fd38083715a820bffd8f62fa9f5
David Strack <davidstrack@gmail.com>
2012-03-27 18:06:38 -0500

    display errors in view

--- single_pages/dashboard/multisite/sites.php
+++ single_pages/dashboard/multisite/sites.php
@@ -1,6 +1,7 @@
 <?php 
 	$form = Loader::helper('form');
 	$fh = Loader::helper('field', 'multisite');
+	$showForm = (empty($sites) || !empty($errors));
 ?>
 <style type="text/css" media="screen">
 	.form-label { width: 130px; }
@@ -10,7 +11,7 @@
 		display: block;
 		margin: 5px 0;
 	}
-	<?php if (!empty($sites)): ?>
+	<?php if (!$showForm): ?>
 		#newSite { display: none; }
 	<?php endif; ?>
 </style>
@@ -23,12 +24,22 @@
 			<a href="<?php echo $this->url('/dashboard/system/seo/urls/') ?>">Click here to enable pretty URLs</a>
 		</div>
 	<?php endif; ?>
+	<?php if ($errors): ?>
+		<div class="alert-message block-message error">
+			<strong>There were some problems saving the site.</strong>
+			<ul style="margin-top: 5px;">
+				<?php foreach ($errors as $e): ?>
+					<li><?php echo $e ?></li>
+				<?php endforeach ?>
+			</ul>
+		</div>
+	<?php endif; ?>
 	<div class="ccm-pane">
 		<div class="ccm-pane-header">
 			<h3>Manage Websites</h3>
 		</div>
 		<div class="ccm-pane-body ccm-pane-body-footer">
-			<?php if (!empty($sites)): ?>
+			<?php if (!$showForm): ?>
 				<div style="margin-bottom: 10px; text-align: right;">
 					<a id="openForm" href="#" class="btn primary">Add New Site</a>
 				</div>
@@ -71,7 +82,7 @@
 						<tr>
 							<th>Home Page</th>						
 							<th>URL</th>
-							<th width="160">Actions</th>
+							<th width="250">Actions</th>
 						</tr>
 					</thead>
 					<tbody>
@@ -81,6 +92,7 @@
 								<td><?php echo $site->url ?></td>
 								<td>
 									<a href="<?php echo $site->getPage()->getCollectionPath() ?>" class="btn">Visit</a>
+									<a href="<?php echo $this->url('/dashboard/multisite/sites/edit', $site->id) ?>" class="btn">Edit Settings</a>
 									<a href="<?php echo $this->action('delete', $site->id) ?>" class="btn danger">Delete</a>
 								</td>
 							</tr>
