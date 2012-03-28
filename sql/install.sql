243c61cfb4871ffcd06670752823a1ad717e5bfd
David Strack <davidstrack@gmail.com>
2012-03-28 09:27:13 -0500

    delete unused sql files

--- sql/install.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `Sites` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `url` varchar(255) NOT NULL,
-  `home_id` int(11) NOT NULL,
-  `favicon` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
