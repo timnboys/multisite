288be2ecd629adadcd501938f2d073485d9155ba
David Strack <davidstrack@gmail.com>
2012-03-28 09:18:59 -0500

    remove unused function

--- helpers/route.php
+++ helpers/route.php
@@ -2,30 +2,7 @@
 defined('C5_EXECUTE') or die(_("Access Denied."));
 
 class RouteHelper {
-	
-	/*
-		Takes a page object and figures out what URL should be used to
-		access that page.
-	*/
-	public static function getUrl($path) {
-		$hostname = $this->getHost();
-		$pattern = '/(\/sites\/'.$hostname.')(.*)/';
-		$match = preg_match($pattern, $path, $matches);
 		
-		if ($hostname != MAIN_URL && $match) {
-			$url = '';
-			foreach ($matches as $m) {
-				if (!preg_match($pattern, $m)) {
-					$url .= $m;
-				}
-			}
-		}
-		else {
-			$url = $path;
-		}
-		return $url;
-	}
-	
 	public static function getHost() {
 		return self::sanitizeUrl($_SERVER['HTTP_HOST']);
 	}
