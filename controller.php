9b7f43e2bdfba6d82ee6bb2047427f7811e9ae40
David Strack <davidstrack@gmail.com>
2012-05-18 10:49:01 -0500

    version++

--- controller.php
+++ controller.php
@@ -6,7 +6,7 @@ class MultisitePackage extends Package {
 	protected $pkgHandle = 'multisite';
 
 	protected $appVersionRequired = '5.5.0.0';
-	protected $pkgVersion = '0.9.3';
+	protected $pkgVersion = '0.9.4';
 	
 	public function getPackageDescription() {
 		return t('Manage multiple websites with ease.');
